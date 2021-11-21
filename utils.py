61e86195bdf40d128073be14c88426851ca645fb
Lorenzo Basile <56440763+lorenzobasile@users.noreply.github.com>
2021-11-21 19:20:30 +0100

    started hw2

---
+++ utils.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def normalize(x):
+    return np.divide((x-x.mean(axis=0)), x.std(axis=0))
+
+def PCA(x, d):
+    N=x.shape[0]
+    cov=x.T@x/N
+    eigenvalues, eigenvectors=np.linalg.eig(cov)
+    sorted_indices = np.argsort(-eigenvalues)
+    eigenvalues = eigenvalues[sorted_indices]
+    eigenvectors = eigenvectors[:,sorted_indices]
+    A=eigenvectors[:,:d]
+    y=x@A
+    return y, eigenvalues, A
+
+def MDS(delta, d):
+    N=delta.shape[0]
+    all_distances=np.sum(delta, axis=0)
+    pair_sums=np.stack([all_distances for i in range(N)], axis=0)+np.stack([all_distances for i in range(N)], axis=1)
+    G=-1/2*(delta+1/(N**2)*np.sum(delta)-1/N*pair_sums)
+    eigenvalues, eigenvectors=np.linalg.eig(G)
+    sorted_indices = np.argsort(-eigenvalues)
+    eigenvalues = eigenvalues[sorted_indices]
+    eigenvectors = eigenvectors[:,sorted_indices]
+    y=np.multiply(eigenvectors[:,:d], np.sqrt(eigenvalues[:d]))
+    return y, eigenvalues
+
+def gaussian_kernel(x_i, x_j, width):
+    return np.exp(-np.linalg.norm(x_i-x_j)**2/(2*width**2))
